7fdf4a4b70b77825db6380785599dc843aa85fb1
Peter Kim <peterckim96@gmail.com>
2018-04-14 19:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var resultArray = [];
   var i, j;
-  console.log(musicians.length);
   for (i = 0; i < musicians.length; i++) {
     var result = musicians[i] + ' plays ' + instruments[i];
     resultArray[i] = result;
