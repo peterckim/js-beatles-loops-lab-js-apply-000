b290572284c627ce0743cc826831c7629ce806aa
Peter Kim <peterckim96@gmail.com>
2018-04-14 19:55:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var emptyarray = [];
-  
+  var i;
+  for (i = 0; i < musicians.length; i++) {
+    var result = musicians[i] + ' ' + instruments
+  }
 }
